0d216f10ae1458cf7d1120a8b029004605a96819
Ethan Cheng <chenget@sheridancollege.ca>
2026-01-28 11:02:45 -0500

    Added a button and text for ui and changed colour and size to turn of square

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIDemo : MonoBehaviour
+{
+    SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            changeColour();
+        }
+    }
+
+    public void changeColour()
+    {
+        sr.color = Random.ColorHSV();
+    }
+
+    public void setSize(float size)
+    {
+        transform.localScale = Vector3.one * size;
+    }
+}
