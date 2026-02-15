cd0c424a3694c369c08779f3ec35e9da67ce0672
DESKTOP-LT61UHN\camera <chenget@sheridancollege.ca>
2026-02-14 22:23:47 -0500

    Made the targets be killed then revived. Added time counter, non functional yet

--- Assets/Scripts/Moving Targets.cs
+++ Assets/Scripts/Moving Targets.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class MovingTargets : MonoBehaviour
@@ -6,9 +7,14 @@ public class MovingTargets : MonoBehaviour
     public float baseSpeed = 0.05f;
     private float direction = 1f;
     public Slider slider;
+    public SpriteRenderer targetHead;
+    public SpriteRenderer targetBody;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+       targetHead = GetComponent<SpriteRenderer>();
+       targetBody = GetComponent<SpriteRenderer>();
        slider.minValue = 0.01f;
        slider.maxValue = 1f;
        slider.value = baseSpeed;
@@ -19,7 +25,7 @@ public class MovingTargets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         float speed = baseSpeed * direction;
         Vector3 newPos = transform.position;
         newPos.y += speed;
@@ -29,9 +35,26 @@ public class MovingTargets : MonoBehaviour
         {
             direction *= -1f;
         }
+
+        if (targetHead.bounds.Contains(mousePos) == true && Mouse.current.leftButton.wasReleasedThisFrame == true)
+        {
+            //is it clicking the button 
+            
+                gameObject.SetActive(false);
+            
+        }
+        else if (targetBody.bounds.Contains(mousePos) == true && Mouse.current.leftButton.wasReleasedThisFrame == true)
+        {
+            gameObject.SetActive(false);
+        }
     }
     public void SetSpeed(float newSpeed)
     {
         baseSpeed = newSpeed;
+    }
+
+    public void Revive()
+    {
+        gameObject.SetActive(true);
     }
 }
