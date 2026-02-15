cc6c11de5ecd596754a80f5215215c2867e396f3
DESKTOP-LT61UHN\camera <chenget@sheridancollege.ca>
2026-02-14 20:12:17 -0500

    Modified speed succsesfully and made offset cursor

--- Assets/Scripts/Moving Targets.cs
+++ Assets/Scripts/Moving Targets.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovingTargets : MonoBehaviour
 {
-    public float speed = 0.001f;
+    public float baseSpeed = 0.05f;
+    private float direction = 1f;
+    public Slider slider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       slider.minValue = 0.01f;
+       slider.maxValue = 1f;
+       slider.value = baseSpeed;
+       slider.wholeNumbers = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float speed = baseSpeed * direction;
         Vector3 newPos = transform.position;
         newPos.y += speed;
         transform.position = newPos;
 
         if (newPos.y > 5.3 || newPos.y < -5)
         {
-            speed = speed * -1;
+            direction *= -1f;
         }
+    }
+    public void SetSpeed(float newSpeed)
+    {
+        baseSpeed = newSpeed;
     }
 }
